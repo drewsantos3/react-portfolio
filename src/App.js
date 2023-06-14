47a0dd214f990f564191bf651117ba9af02dc38a
Drew S <andrewsantos2022@u.northwestern.edu>
2023-06-14 04:32:14 -0500

    bug fixes

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         <div className="all dark">
           {/* <button onClick={toggleTheme}>Toggle Theme</button> */}
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/react-portfolio" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/projects" element={<Projects />} />
