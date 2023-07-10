eba6754559d87cbfd2fdf9ea0be756ce94a94982
Drew S <andrewsantos2022@u.northwestern.edu>
2023-07-10 17:15:44 -0500

    finished contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,26 +1,41 @@
-import { Link } from 'react-router-dom';
+
 import Header from '../Header';
 import Footer from '../Footer';
+import ghLogo from '../../assets/images/github-mark.png';
+import liLogo from '../../assets/images/linkedin.png';
 
 const Contact = () => {
     return (
         <div>
         <Header />
-        <div className="contact">
-            <h1>Contact Me</h1>
-            <p>
-            If you'd like to get in touch with me, please feel free to reach out to me via email at <a href="mailto: drewsantos3@gmail.com">drewsantos3@gmail.com</a> or by phone at <a href="tel: 312-521-9832">312-521-9832</a>
-            </p>
-            <p>
-            You can also find me on <a href="https://www.linkedin.com/in/andrew-santos-5a1372247/" target="_blank" rel="noreferrer">LinkedIn</a>
-            </p>
-            <p>
-            <Link to="/">Back to Home</Link>
-            </p>
+        <section id="contact" className='text-white'>
+            <h2 className='text-center p-5 text-2xl'>Contact Me</h2>
+            <div className="pt-20 flex flex-col">
+          <div className="myinfo flex justify-center">
+            <div>
+              <p className="text-2xl font-bold text-right p-4">EMAIL:</p>
+              <p className="text-2xl font-bold text-right p-4">PHONE:</p>
+              <p className="text-2xl font-bold text-right p-4">CITY, STATE, ZIP:</p>
+            </div>
+            <div>
+              <p className="text-2xl text-left p-4">drewsantos3@gmail.com</p>
+              <p className="text-2xl text-left p-4">312-521-9832</p>
+              <p className="text-2xl text-left p-4">Chicago, Illinois 60611</p>
+            </div>
+          </div>
+          <div className="flex w-full justify-center">
+            <a href="https://github.com/drewsantos3" className="text-2xl text-left p-4">
+              <img src={ghLogo} alt='github logo' className="h-20 w-20"></img>
+            </a>
+            <a href="https://www.linkedin.com/in/drew-santos1" className="text-2xl text-right p-4">
+              <img src={liLogo} alt='linkedin logo' className="h-20 w-20"></img>
+            </a>
+          </div>
         </div>
-        <Footer />
-        </div>
-    )
-}
+      </section>
+       <Footer />
+    </div>
+  );
+};
 
 export default Contact;
