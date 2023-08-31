e1c6cd9a2f375c072b3a1b064df6d9c8aade3fd5
Drew S <andrewsantos2022@u.northwestern.edu>
2023-08-31 13:45:46 -0500

    bug fixes

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function mobileNav() {
   return (
     <div className="mobileNav">
-        <nav className="m-6 flex flex-col gap-2">
+        <nav className="flex flex-col gap-2">
             <Link to="/about" className="navlink px-5 text-xl text-white">
                 ABOUT ME
             </Link>
