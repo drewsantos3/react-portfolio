31b34c3ad8c08eeba23ad1edb1467ac1801327c8
Drew S <andrewsantos2022@u.northwestern.edu>
2024-01-02 16:28:23 -0600

    updated readme and index.html, cleaned up code

--- README.md
+++ README.md
@@ -3,15 +3,17 @@
 ## Table of Contents
 * [Description](#description)
 * [Usage](#usage)
+* [Screenshot](#screenshot)
 * [License](#license)
 * [Questions](#questions)
 
 ## Description
-This is my coding portfolio built with react.js. It is a single page application that uses react-router-dom to navigate between pages. I am constantly updating this portfolio with new projects and skills as I learn them. This application is deployed on GitHub Pages.
+This is my coding portfolio built with react.js. It uses react-router-dom to navigate between pages and is styled with tailwinds as well as custom CSS. I am constantly updating this portfolio with new projects, skills, and features as I learn them. Application is deployed via GitHub Pages.
 
 ## Usage
 [Click here to view the deployed portfolio](https://drewsantos3.github.io/react-portfolio/)
 
+## Screenshot
 ![Screenshot of deployed portfolio](/src/assets/images/screenshot.png)
 
 ## License
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -37,52 +37,6 @@ const projects = [
     github: "https://github.com/drewsantos3/e-commerce-backend.git",
   }
 ];
-//          <img src={Attak} alt="Attak" />
-//           <div className="project-info">
-//             <h2>Attak Tracker</h2>
-//             <p>
-//               Attak Tracker is a full stack web application that allows users to create
-//               workouts. Users can create workouts and add exercises to those workouts. The application is built using React, Node, Express, and MongoDB.
-//             </p>
-//             <a
-//               href="https://attak-tracker.herokuapp.com/login"> click here for deployed app</a>
-//             <a
-//               href="https://github.com/attak5/attak-tracker.git">click here for github repo</a>
-//             </div>
-//         </div>
-//         <div className="project">
-//             <img src={Bikes} alt="Bikes4U" />
-//             <div className="project-info">
-//                 <h2>Bikes4U</h2>
-//                 <p>
-//                     Bikes4U is a full stack web application that allows users to locate nearby bike stations in their local area.
-//                      Whether a user wishes to rent or return a bike, there has never before been a more streamlined method to find and navigate to a station that will contain the amount of needed bikes or empty slots. This will, in turn, aid in reducing emissions in metropolitan areas across the globe.
-//                 </p>
-//                 <a 
-//                 href="https://team-a2c2e.github.io/Bikes4U/">click here for deployed app</a>
-//                 <a
-//                 href="https://github.com/Team-A2C2E/Bikes4U.git">click here for github repo</a>
-//             </div>
-//         </div>
-//         <div className="project">
-//             <img src={Ocean} alt="Ocean Buddies" />
-//             <div className="project-info">
-//                 <h2>Ocean Buddies</h2>
-//                 <p>
-//                 A website for a ocean preservation charity that allows you to donate, volunteer, or purchase items with all money going towards the charity. The website includes a news section if you log in!
-//                 </p>
-//                 <a
-//                 href="https://ocean-buddies.herokuapp.com/">click here deployed app</a>
-//                 <a
-//                 href="https://github.com/Oceans-4/Ocean-App.git">click here github repo</a>
-//             </div>
-//         </div>
-//         </main>
-//         <Footer />
-//     </div>
-
-//     );
-// };
 
 export default function Project() {
   return (
